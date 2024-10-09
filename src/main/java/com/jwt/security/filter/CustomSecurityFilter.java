b216f7946498f2798c0b46dfa1eacc1e613308c0
Eduardo Fontenele <eduardofontedev@gmail.com>
2024-10-09 00:10:59 -0300

    forgot unused import

--- src/main/java/com/jwt/security/filter/CustomSecurityFilter.java
+++ src/main/java/com/jwt/security/filter/CustomSecurityFilter.java
@@ -8,7 +8,6 @@ import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
